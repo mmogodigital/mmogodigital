75842880d13b1c1db034f76c8d14c121ffaa5125
Godwill <kglikius@gmail.com>
2017-03-08 08:50:27 +0200

    Moved Formspree to ajax

--- static/js/front.js
+++ static/js/front.js
@@ -2,18 +2,12 @@
 /* eslint no-unused-vars: ["error", { "varsIgnorePattern": "animationsSlider" }] */
 
 $(function () {
-  fullScreenContainer()
-  productDetailGallery(4000)
-  productDetailSizes()
-  utils()
-  animations()
-  counters()
   contactForm()
-})
+});
 
 // Ajax contact
 function contactForm () {
-  var form = $('.contact-form')
+  var form = $('.contact-form');
   form.submit(function () {
     $this = $(this)
     $.post($(this).attr('action'),
@@ -25,213 +19,10 @@ function contactForm () {
         .html('<div class="alert alert-success" role="alert"><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">×</span><span class="sr-only">Close</span></button>Thank you for getting in touch. We will get back to you soon!</div>')
         .fadeIn()
       }
-      , 'json')
+      , 'json');
     return false
   })
 }
-
-/* animations */
-function animations () {
-  var delayTime = 0
-  $('[data-animate]').css({opacity: '0'})
-  $('[data-animate]').waypoint(function () {
-    delayTime += 150
-    $(this).delay(delayTime).queue(function (next) {
-      $(this).toggleClass('animated')
-      $(this).toggleClass($(this).data('animate'))
-      delayTime = 0
-      next()
-      // $(this).removeClass('animated')
-      // $(this).toggleClass($(this).data('animate'))
-    })
-  }, {
-    offset: '90%',
-    triggerOnce: true
-  })
-
-  $('[data-animate-hover]').hover(function () {
-    $(this).css({opacity: 1})
-    $(this).addClass('animated')
-    $(this).removeClass($(this).data('animate'))
-    $(this).addClass($(this).data('animate-hover'))
-  }, function () {
-    $(this).removeClass('animated')
-    $(this).removeClass($(this).data('animate-hover'))
-  })
-}
-
-
-/* counters */
-function counters () {
-  $('.counter').counterUp({
-    delay: 10,
-    time: 1000
-  })
-}
-
-/* picture zoom */
-function pictureZoom () {
-  $('.product .image, .post .image, .photostream div').each(function () {
-    var imgHeight = $(this).find('img').height()
-    $(this).height(imgHeight)
-  })
-}
-
-/* full screen intro */
-function fullScreenContainer () {
-  var screenWidth = $(window).width() + 'px'
-  var screenHeight = '500px'
-
-  if ($(window).height() > 500) {
-    screenHeight = $(window).height() + 'px'
-  }
-
-  $('#intro, #intro .item').css({
-    width: screenWidth,
-    height: screenHeight
-  })
-}
-
-function utils () {
-  /* tooltips */
-  $('[data-toggle="tooltip"]').tooltip()
-
-  /* click on the box activates the radio */
-  $('#checkout').on('click', '.box.shipping-method, .box.payment-method', function () {
-    var radio = $(this).find(':radio')
-    radio.prop('checked', true)
-  })
-
-  /* click on the box activates the link in it */
-  $('.box.clickable').on('click', function () {
-    window.location = $(this).find('a').attr('href')
-  })
-
-  /* external links in new window */
-  $('.external').on('click', function (e) {
-    e.preventDefault()
-    window.open($(this).attr('href'))
-  })
-
-  /* animated scrolling */
-  $('.scroll-to, .scroll-to-top').click(function (event) {
-    var fullUrl = this.href
-    var parts = fullUrl.split('#')
-
-    if (parts.length > 1) {
-      scrollTo(fullUrl)
-      event.preventDefault()
-    }
-  })
-
-  function scrollTo (fullUrl) {
-    var parts = fullUrl.split('#')
-    var trgt = parts[1]
-    var targetOffset = $('#' + trgt).offset()
-    var targetTop = targetOffset.top - 100
-
-    if (targetTop < 0) {
-      targetTop = 0
-    }
-
-    $('html, body').animate({
-      scrollTop: targetTop
-    }, 1000)
-  }
-}
-
-/* product detail gallery */
-function productDetailGallery (confDetailSwitch) {
-  $('.thumb:first').addClass('active')
-  var timer = setInterval(autoSwitch, confDetailSwitch)
-
-  $('.thumb').click(function (e) {
-    switchImage($(this))
-    clearInterval(timer)
-    timer = setInterval(autoSwitch, confDetailSwitch)
-    e.preventDefault()
-  })
-
-  $('#mainImage').hover(function () {
-    clearInterval(timer)
-  }, function () {
-    timer = setInterval(autoSwitch, confDetailSwitch)
-  })
-
-  function autoSwitch () {
-    var nextThumb = $('.thumb.active').closest('div').next('div').find('.thumb')
-    if (nextThumb.length === 0) {
-      nextThumb = $('.thumb:first')
-    }
-    switchImage(nextThumb)
-  }
-
-  function switchImage (thumb) {
-    $('.thumb').removeClass('active')
-    var bigUrl = thumb.attr('href')
-    thumb.addClass('active')
-    $('#mainImage img').attr('src', bigUrl)
-  }
-}
-
-/* product detail sizes */
-function productDetailSizes () {
-  $('.sizes a').click(function (e) {
-    e.preventDefault()
-    $('.sizes a').removeClass('active')
-    $('.size-input').prop('checked', false)
-    $(this).addClass('active')
-    $(this).next('input').prop('checked', true)
-  })
-}
-
-$.fn.alignElementsSameHeight = function () {
-  $('.same-height-row').each(function () {
-    var maxHeight = 0
-    var children = $(this).find('.same-height')
-    children.height('auto')
-
-    if ($(window).width() > 768) {
-      children.each(function () {
-        if ($(this).innerHeight() > maxHeight) {
-          maxHeight = $(this).innerHeight()
-        }
-      })
-      children.innerHeight(maxHeight)
-    }
-
-    maxHeight = 0
-    children = $(this).find('.same-height-always')
-    children.height('auto')
-    children.each(function () {
-      if ($(this).height() > maxHeight) {
-        maxHeight = $(this).innerHeight()
-      }
-    })
-    children.innerHeight(maxHeight)
-  })
-}
-
-var windowWidth
-$(window).load(function () {
-  windowWidth = $(window).width()
-
-  $(this).alignElementsSameHeight()
-  pictureZoom()
-})
-
-$(window).resize(function () {
-  var newWindowWidth = $(window).width()
-
-  if (windowWidth !== newWindowWidth) {
-    setTimeout(function () {
-      $(this).alignElementsSameHeight()
-      fullScreenContainer()
-      pictureZoom()
-    }, 205)
-    windowWidth = newWindowWidth
-  }
-})
 
 
 $('#main-slider .slider').slick({
@@ -291,4 +82,16 @@ $( "#bw-office" ).click(function(e) {
     e.preventDefault();
     $('#map-block').removeClass("hidden");
     $('#za-map-block').addClass("hidden");
+});
+
+$( ".contact-form button" ).click(function(e) {
+    var data = $('form.contact-form').serializeArray();
+
+    $.ajax({
+        url: "https://formspree.io/sales@mmogodigital.com",
+        method: "POST",
+        data: {message: data},
+        dataType: "json"
+    });
+
 });
